7875e261dec481588951e0301bf3be80b731afa6
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2022-11-11 02:08:30 -0600

    work on my second cli tool

--- Python_CLI_Tools/scratch_files_tool/scratch_file.py
+++ Python_CLI_Tools/scratch_files_tool/scratch_file.py
@@ -1,9 +1,79 @@
-import click
+import click, os, json
 
-# This is just a placeholder for the second tool I'll be building with Click
+def get_data(file_path) -> dict:
+    new_data = {}
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+        if data is None:
+            with open(f, "w") as f:
+                json.dump(new_data, f)
+            with open(file_path, "r") as f:
+                data = json.load(f)
+            return data
+        else:
+            return data
+    else:
+        with open(file_path, "w") as f:
+            json.dump(new_data, f)
+        with open(file_path, "r") as f:
+            data = json.load(f)
+        return data
+
+def get_key_choice(choices) -> int:
+    choice = int(input("Which item would you like to view?\n> "))
+    while choice < 0 and choice > choices:
+        print("Invalid selection!")
+        choice = int(input("Which item would you like to view?\n> "))
+    return choice - 1
+
+def get_user_file_choice():
+    options = [
+        {"choice": "x", "desc": "Exit" },
+        {"choice": "v", "desc": "View: View a specific entry" },
+        {"choice": "n", "desc": "New: Create a new entry" },
+        {"choice": "d", "desc": "Delete: Delete an entry" },
+        {"choice": "e", "desc": "Edit: Edit an entry" }
+    ]
+    for option in options:
+        opt_choice = option['choice']
+        opt_desc = option['desc']
+        print(f"'{opt_choice}' : {opt_desc}")
+
+
 @click.command()
-def scratch_file():
-    print("you've called the Scratch File CLI Tool!!!")
+@click.option('-f', required=False, help="the file where your scratch files are stored")
+def scratch_file(f=''):
+
+    # create a default value
+    # if none is passed
+    if f is None:
+        f = "./scratch_files.json"
+
+    data: dict = get_data(f)
+    keys = data.keys()
+
+    if len(keys) == 0:
+        print("Nothing to show yet")
+
+    else:
+        print("Current items:\n")
+        items = []
+        for i, key in enumerate(keys):
+            print(i+1, key, sep=" : ")
+            items.append(key)
+
+        file_choice = get_user_file_choice()
+
+    while file_choice != "x":
+        if file_choice is "v":
+            choice = get_key_choice(len(keys))
+            selection = data.get(items[choice])
+            print(items[choice], selection, sep=": \n")
+            file_choice = get_user_file_choice()
+
+
+
 
     # create a check for a file
         # if theirs not a file location prompt the user to add a new scratch file from scratch
